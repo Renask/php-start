ae227201864463bc05eacea1fa7aee01dcff0db5
Renask <rkhalil@roc-dev.com>
2017-03-30 10:47:52 +0200

    Add files via upload

---
+++ kerstboom.php
@@ -0,0 +1,16 @@
+<center>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renas
+ * Date: 30-3-2017
+ * Time: 10:17
+ */
+$regel = 10;
+
+    for($i = 1; $i <= $regel; $i++){
+        for($j = 0; $j <$i ;$j++){
+            echo "*";
+        }
+        echo "<br>";
+    }
