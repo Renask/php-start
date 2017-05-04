8afa04b239525096cc6608004b5329ca2e79a08d
Renask <rkhalil@roc-dev.com>
2017-05-04 11:09:17 +0200

    Add files via upload

---
+++ Kapperszaak.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renas
+ * Date: 6-4-2017
+ * Time: 11:05
+ */
+$kappersagenda["09:15"] = "Mevr. pietersen";
+$kappersagenda["09:30"] = "Mevr. Willems";
+$kappersagenda["09:45"] = "";
+$kappersagenda["10:00"] = "Paul van den Broek";
+$kappersagenda["10:15"] = "karel de meeuw";
+$kappersagenda["10:30"] = "";
+
+
+
+
+foreach($kappersagenda as $tijd=>$naam) {
+    if($afspraak == "") {
+        print("<li>".$tijd."</li>") ;
+    }
+
+    }
+
+
+
+
+
+
+
+
